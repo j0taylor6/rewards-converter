fc2e440b0e660c069c05760242ba5e3958423a14
Josh Taylor <149825449+j0taylor6@users.noreply.github.com>
2023-12-26 23:14:25 +0000

    Update RewardValueTests.java

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -1,5 +1,4 @@
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RewardValueTests {
@@ -7,30 +6,30 @@ public class RewardValueTests {
     @Test
     void create_with_cash_value() {
         double cashValue = 100;
-        var rewardValue = new RewardValue(cashValue);
-        assertEquals(cashValue, rewardValue.getCashValue());
+        RewardValue rewardValue = new RewardValue(cashValue);
+        assertEquals(cashValue, rewardValue.getCashValue(), "Cash value not set correctly in constructor.");
     }
 
     @Test
     void create_with_miles_value() {
         int milesValue = 10000;
-        var rewardValue = new RewardValue(milesValue);
-        assertEquals(milesValue, rewardValue.getMilesValue());
+        RewardValue rewardValue = new RewardValue(milesValue);
+        assertEquals(milesValue, rewardValue.getMilesValue(), "Miles value not set correctly in constructor.");
     }
 
     @Test
     void convert_from_cash_to_miles() {
         double cashValue = 100;
-        var rewardValue = new RewardValue(cashValue);
-        int expectedMilesValue = (int) (cashValue / 0.0035);
-        assertEquals(expectedMilesValue, rewardValue.getMilesValue());
+        double expectedMiles = cashValue / 0.0035;
+        RewardValue rewardValue = new RewardValue(cashValue);
+        assertEquals(expectedMiles, rewardValue.getMilesValue(), "Conversion from cash to miles is incorrect.");
     }
 
     @Test
     void convert_from_miles_to_cash() {
         int milesValue = 10000;
-        var rewardValue = new RewardValue(milesValue);
-        double expectedCashValue = milesValue * 0.0035;
-        assertEquals(expectedCashValue, rewardValue.getCashValue());
+        double expectedCash = milesValue * 0.0035;
+        RewardValue rewardValue = new RewardValue(milesValue);
+        assertEquals(expectedCash, rewardValue.getCashValue(), "Conversion from miles to cash is incorrect.");
     }
 }
